47d904a56f2270497a62219e2e69cf0b15c6c6f4
JRMarketing777 <158335051+JRMarketing777@users.noreply.github.com>
2024-05-10 20:24:48 -1000

    Update Stock.py

--- Hello.py
+++ Hello.py
@@ -1,51 +1,34 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+import yfinance as yf
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+import numpy as np
 
-LOGGER = get_logger(__name__)
+# Set up the page
+st.title('Stock Price Prediction Dashboard')
+st.sidebar.header('User Input Parameters')
 
+def user_input_features():
+    ticker = st.sidebar.text_input('Ticker', 'AAPL')
+    return ticker
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+ticker = user_input_features()
+data = yf.download(ticker, '2020-01-01', '2023-01-01')
 
-    st.write("# Welcome to Streamlit! 👋")
+st.line_chart(data.Close)
 
-    st.sidebar.success("Select a demo above.")
+# Prepare data for ML model
+data['SMA'] = data['Close'].rolling(window=14).mean()
+data = data.dropna()
+X = np.array(data.index).reshape(-1, 1)
+y = data['SMA'].values
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+# Build and train the model
+model = LinearRegression()
+model.fit(X, y)
 
+# Prediction and plot
+prediction = model.predict(X)
+st.write('Model Prediction of Moving Average Prices')
+st.line_chart(data.index, prediction)
 
-if __name__ == "__main__":
-    run()
